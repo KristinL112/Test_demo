c915c9b119fe81100234bf4f472c04078e4db293
KristinL112 <144241150+KristinL112@users.noreply.github.com>
2024-02-04 13:49:07 +0100

    påbörjat appen

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import 'bootsrap/dist/css/bootsrap.css'
+
+import bootstrap from 'bootsrap/dist/css/bootsrap.bundle.js'
 
 import App from './App.vue'
 import router from './router'
@@ -11,4 +15,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(bootstrap)
 app.mount('#app')
